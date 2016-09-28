dcdb16002d6aca44943a80b671cb3e05bbf5d99c
geovannilei <geovanni3557@gmail.com>
2016-09-27 19:16:05 -0500

    Add files via upload

---
+++ scrips/loadPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class loadPlayer : MonoBehaviour {
+    private List<GameObject> models;
+
+    void Start () {
+        models = new List<GameObject>();
+        foreach (Transform t in transform)
+        {
+            models.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+        models[selector.playerint()].SetActive(true);
+    }
+}
